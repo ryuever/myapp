1a416f1aa3af0204591cc674de41643adc94dcbd
ryuever <liuyouchao111@gmail.com>
2013-06-24 06:57:41 +0900

    validation avatar

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -20,10 +20,10 @@ class Attachment < ActiveRecord::Base
   
 #  validates_attachment_presence :avatar
 
-#  has_attached_file :avatar, 
-                  #   :url  => "/system/pictures/:id/:style_:basename.:extension", 
-                  #   :path => ":rails_root/public/system/pdf/:id/:style_:basename.:extension", 
-                  # :styles => { :medium => "300x300>", :thumb => "100x100>" } 
+ has_attached_file :avatar, 
+                    :url  => "/system/pictures/:id/:style_:basename.:extension", 
+                    :path => ":rails_root/public/system/pdf/:id/:style_:basename.:extension", 
+                  :styles => { :medium => "300x300>", :thumb => "100x100>" } 
 # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   # def check_content_type
@@ -32,9 +32,9 @@ class Attachment < ActiveRecord::Base
   #   end
   # end
 
-  has_attached_file :avatar, :styles => { :small => "60x60>", :thumb => "60x60>" }
+  # has_attached_file :avatar, :styles => { :small => "60x60>", :thumb => "60x60>" }
 
-  validates_attachment :avatar, :presence => true, 
-  :content_type => { :content_type => "image/jpg" }, 
-  :size => { :in => 0..1000.kilobytes }  
+  # validates_attachment :avatar, :presence => true, 
+  # :content_type => { :content_type => "image/jpg" }, 
+  # :size => { :in => 0..1000.kilobytes }  
 end
